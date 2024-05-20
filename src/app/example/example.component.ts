945c548d67c544444d314d951e9dec152a34a673
Evan Marie Carr <evanmariecarr@gmail.com>
2024-05-19 23:16:05 -0400

    initial commit

---
+++ src/app/example/example.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  standalone: true,
+  imports: [],
+  templateUrl: './example.component.html',
+  styleUrl: '/global.css'
+})
+export class ExampleComponent {
+
+}
