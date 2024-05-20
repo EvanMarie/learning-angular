85ecf38603f84bec0cc222db4d63f7aaefe3df77
Evan Marie Carr <evanmariecarr@gmail.com>
2024-05-20 13:04:22 -0400

    learning, learning, learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ExampleComponent } from './example/example.component';
-import { FlexComponent } from './building-blocks/flex.component';
+import { CenterHorizontal, FlexFull, VStackFit, VStackFull, FlexFit } from './building-blocks/flexboxes.component';
 
 
 @Component({
@@ -9,7 +9,7 @@ import { FlexComponent } from './building-blocks/flex.component';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ExampleComponent, FlexComponent]
+    imports: [RouterOutlet, ExampleComponent, FlexFull, CenterHorizontal, VStackFit, VStackFull, FlexFit]
 })
 export class AppComponent {
   title = 'learning-angular';
---
+++ src/app/building-blocks/flexboxes.component.ts
@@ -0,0 +1,77 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// FlexFit
+@Component({
+  selector: 'app-flex-fit',
+  template: `
+    <div class="flex w-fit" [ngClass]="customClass">
+      <ng-content></ng-content>
+    </div>
+  `,
+  standalone: true,
+  imports: [CommonModule]
+})
+export class FlexFit {
+  @Input() customClass: string = '';
+}
+
+// FlexFull
+@Component({
+  selector: 'app-flex-full',
+  template: `
+    <div class="flex" [ngClass]="customClass">
+      <ng-content></ng-content>
+    </div>
+  `,
+  standalone: true,
+  imports: [CommonModule]
+})
+export class FlexFull {
+  @Input() customClass: string = '';
+}
+
+// CenterHorizontal
+@Component({
+  selector: 'app-center-horizontal',
+  template: `
+    <div class="flex w-full justify-center items-center" [ngClass]="customClass">
+      <ng-content></ng-content>
+</div>
+  `,
+  standalone: true,
+  imports: [CommonModule]
+})
+export class CenterHorizontal {
+  @Input() customClass: string = '';
+}
+
+// VStackFit
+@Component({
+  selector: 'app-vstack-fit',
+  template: `
+    <div class="flex flex-col w-fit" [ngClass]="customClass">
+      <ng-content></ng-content>
+    </div>
+  `,
+  standalone: true,
+  imports: [CommonModule]
+})
+export class VStackFit {
+  @Input() customClass: string = '';
+}
+
+// VStackFull
+@Component({
+  selector: 'app-vstack-full',
+  template: `
+    <div class="flex flex-col w-full" [ngClass]="customClass">
+      <ng-content></ng-content>
+    </div>
+  `,
+  standalone: true,
+  imports: [CommonModule]
+})
+export class VStackFull {
+  @Input() customClass: string = '';
+}
