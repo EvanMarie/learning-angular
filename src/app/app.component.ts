6fd614895b65cb0eff9d14f67bcce910f1ca790d
Evan Marie Carr <evanmariecarr@gmail.com>
2024-05-20 12:21:03 -0400

    learning to make small custom components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ExampleComponent } from './example/example.component';
+import { FlexComponent } from './building-blocks/flex.component';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ExampleComponent]
+    imports: [RouterOutlet, ExampleComponent, FlexComponent]
 })
 export class AppComponent {
   title = 'learning-angular';
