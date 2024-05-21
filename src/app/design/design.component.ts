7719f982b21e62138b06df20cb09edfa1bb6d628
Evan Marie Carr <evanmariecarr@gmail.com>
2024-05-21 14:19:34 -0400

    learning to map

--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-design',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './design.component.html',
   styleUrl: './design.component.css'
 })
 export class DesignComponent {
+    themes = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+
 
 }
