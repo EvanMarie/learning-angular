7c3682cc7ed7b033390c72b075994412c0d199ab
Evan Marie Carr <evanmariecarr@gmail.com>
2024-05-21 13:36:09 -0400

    routes and buttons really starting to get this

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DesignComponent } from './design/design.component';
+import { RouteTwoComponent } from './route-two/route-two.component';
+import { RouteThreeComponent } from './route-three/route-three.component';
 
 const routes: Routes = [
 //   { path: '', component: AppComponent },
   { path: 'design', component: DesignComponent },
+    { path: 'route-two', component: RouteTwoComponent },
+       { path: 'route-three', component: RouteThreeComponent },
 ];
 
 export default routes;
